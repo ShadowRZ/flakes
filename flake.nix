89adfe58f5fa32042cb56e6d59005ab894b6e44c
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-02-04 19:39:26 +0800

    Remove subflake

--- flake.nix
+++ flake.nix
@@ -24,51 +24,51 @@
     berberman.url = "github:berberman/flakes";
     berberman.inputs.nixpkgs.follows = "nixpkgs";
     berberman.inputs.nvfetcher.follows = "nvfetcher";
-    ## @ShadowRZ's private flake.
-    shadowrz.url = "path:./pkgs";
-    shadowrz.inputs.nixpkgs.follows = "nixpkgs";
-    shadowrz.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = inputs@{ self, home-manager, nixpkgs, ... }: {
-    # NixOS configurations.
-    nixosConfigurations.hermitmedjed-s = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nixos/configuration.nix
-        # Home Manager Module
-        home-manager.nixosModules.home-manager
-        # (modulesPath + "/installer/scan/not-detected.nix")
-        nixpkgs.nixosModules.notDetected
-        {
-          # Overlays
-          nixpkgs.overlays = [
-            # Neovim Nightly
-            inputs.neovim-nightly.overlay
-            # Users' flake
-            inputs.nickcao.overlay
-            inputs.berberman.overlay
-            inputs.shadowrz.overlay
-            # Fix SmartDNS
-            (final: prev: {
-              smartdns = prev.smartdns.overrideAttrs
-                (attrs: { postPatch = "rm systemd/smartdns.service"; });
-            })
-          ];
-          # Configuration revision.
-          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-          # Home Manager.
-          home-manager = {
-            useUserPackages = true;
-            useGlobalPkgs = true;
-          };
-          # Pin NIX_PATH
-          nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
-          nix.registry.p.flake = self;
-          nix.registry.nixpkgs.flake = nixpkgs;
-          nix.registry.shadowrz.flake = inputs.shadowrz;
-        }
-      ];
-    };
-  };
+  outputs = inputs@{ self, home-manager, nixpkgs, flake-utils, ... }:
+    {
+      overlay = final: prev: (import ./pkgs prev);
+      # NixOS configurations.
+      nixosConfigurations.hermitmedjed-s = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+          # Home Manager Module
+          home-manager.nixosModules.home-manager
+          # (modulesPath + "/installer/scan/not-detected.nix")
+          nixpkgs.nixosModules.notDetected
+          {
+            # Overlays
+            nixpkgs.overlays = [
+              # Neovim Nightly
+              inputs.neovim-nightly.overlay
+              # Users' flake
+              inputs.nickcao.overlay
+              inputs.berberman.overlay
+              self.overlay
+              # Fix SmartDNS
+              (final: prev: {
+                smartdns = prev.smartdns.overrideAttrs
+                  (attrs: { postPatch = "rm systemd/smartdns.service"; });
+              })
+            ];
+            # Configuration revision.
+            system.configurationRevision =
+              nixpkgs.lib.mkIf (self ? rev) self.rev;
+            # Home Manager.
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+            };
+            # Pin NIX_PATH
+            nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+            nix.registry.p.flake = self;
+            nix.registry.nixpkgs.flake = nixpkgs;
+          }
+        ];
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { packages = (import ./pkgs pkgs); });
 }
