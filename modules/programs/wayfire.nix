5b02e3b8e61720f1786c5bac5109ccb5f07cd780
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-03-20 20:13:12 +0800

    Fix Wayfire session startup

--- modules/programs/wayfire.nix
+++ modules/programs/wayfire.nix
@@ -4,6 +4,23 @@ with lib;
 
 let
   cfg = config.programs.wayfire;
+  wayfireSession = pkgs.stdenvNoCC.mkDerivation { 
+    name = "wayfire-session";
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir -p $out/share/wayland-sessions
+      cat > $out/share/wayland-sessions/wayfire.desktop << EOF
+      [Desktop Entry]
+      Name=Wayfire
+      Exec=${pkgs.wayfire}/bin/wayfire
+      TryExec=${pkgs.wayfire}/bin/wayfire
+      Icon=
+      Type=Application
+      DesktopNames=Wayfire
+      EOF
+    '';
+    passthru.providedSessions = ["wayfire"];
+  };
 in {
   options.programs.wayfire = {
     enable = mkEnableOption ''
@@ -17,15 +34,7 @@ in {
     security.polkit.enable = true;
     hardware.opengl.enable = mkDefault true;
     # To make a Wayfire session available if a display manager like SDDM is enabled:
-    services.xserver.displayManager.session = [
-      {
-        manage = "window";
-        name = "wayfire";
-        start = ''
-          ${pkgs.wayfire}/bin/wayfire
-        '';
-      }
-    ];
+    services.xserver.displayManager.sessionPackages = [ wayfireSession ];
     programs.xwayland.enable = mkDefault true;
     # For screen sharing (this option only has an effect with xdg.portal.enable):
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
