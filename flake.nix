a173c5fcb498b5d51425cb46af561b55d2b028e9
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-03-28 16:17:08 +0800

    Bump

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     # Home Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
