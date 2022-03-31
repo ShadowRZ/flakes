55447a11f26ec99ee611954d4f89dc753578c2ea
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-03-31 17:11:03 +0800

    pkgs: Add sddm-sugar-candy

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@ pkgs: {
   kose-font = pkgs.callPackage ./kose-font { };
   adw-gtk3 = pkgs.callPackage ./adw-gtk3 { };
   sddm-lain-wired-theme = pkgs.callPackage ./sddm-lain-wired-theme {};
+  sddm-sugar-candy = pkgs.callPackage ./sddm-sugar-candy {};
 }
