6e62bfe09fdbe045d4fdaf07d6875ee6a71cc222
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-03-31 15:38:38 +0800

    Update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 pkgs: {
-  kose-font = pkgs.callPackage ./data/fonts/kose-font { };
-  adw-gtk3 = pkgs.callPackage ./data/themes/adw-gtk3 { };
+  kose-font = pkgs.callPackage ./kose-font { };
+  adw-gtk3 = pkgs.callPackage ./adw-gtk3 { };
+  sddm-lain-wired-theme = pkgs.callPackage ./sddm-lain-wired-theme {};
 }
