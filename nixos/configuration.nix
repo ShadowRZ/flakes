14720688200e0ec278fe76f1c6afcd84a912ea3c
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-08-03 19:47:27 +0800

    Update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,6 +75,8 @@
       emacsPgtkNativeComp # Emacs with Pure GTK + Native Compilation.
       feeluown # FeelUOwn
       mindustry-wayland # Mindustry (Wayland)
+      nheko # Nheko
+      qownnotes # QOwnNotes
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH2Y7fSAJgH4KJZYsKJo01SVCCoV0A4wmD0etDM394PO u0_a203@localhost"
