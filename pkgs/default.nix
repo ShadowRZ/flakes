71338826f261625e3246eb7fdd067733189721fc
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-08-04 11:01:10 +0800

    pkgs: mkxp-z: init at 2.4.0

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
   adw-gtk3 = pkgs.callPackage ./adw-gtk3 { };
   sddm-lain-wired-theme = pkgs.callPackage ./sddm-lain-wired-theme { };
   sddm-sugar-candy = pkgs.callPackage ./sddm-sugar-candy { };
+  mkxp-z = pkgs.callPackage ./mkxp-z {};
 }
