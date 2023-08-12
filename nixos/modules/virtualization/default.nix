a664a22d05702604d876e8616a0aa7ace548d78b
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-08-12 10:21:13 +0800

    Add libvirtd

--- nixos/modules/virtualization/default.nix
+++ nixos/modules/virtualization/default.nix
@@ -1,4 +1,22 @@
 { pkgs, ... }: {
-  # QEMU
-  environment.systemPackages = with pkgs; [ qemu ];
+  # Libvirtd
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      # Enable UEFI
+      ovmf = {
+        enable = true;
+        packages = [ pkgs.OVMFFull.fd ];
+      };
+      # Enable virtual TPM support
+      swtpm.enable = true;
+    };
+  };
+  # Libvirt tools
+  environment.systemPackages = with pkgs; [
+    virt-viewer
+    virt-manager
+    gnome.gnome-boxes
+  ];
 }
