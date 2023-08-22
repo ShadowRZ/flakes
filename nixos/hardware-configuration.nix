238e41e6d0e58c17ff03cd5862a0a6673eb07aeb
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-08-22 16:25:43 +0800

    Add swap

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -35,6 +35,9 @@
     options = [ "subvolid=257" "compress-force=zstd" ];
   };
 
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/8677f316-df6e-4459-ab19-04572d624d9b"; }];
+
   boot.initrd.luks.devices."luks-e9804028-c8df-4b5b-8557-9aaac7e363d9".device =
     "/dev/disk/by-uuid/e9804028-c8df-4b5b-8557-9aaac7e363d9";
 
