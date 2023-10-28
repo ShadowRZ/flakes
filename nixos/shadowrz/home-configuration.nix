0e93d9bc7f4938a0a43de9c56d697a6ad0715ea9
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-10-28 14:06:10 +0800

    Add Emacs Config

--- nixos/shadowrz/home-configuration.nix
+++ nixos/shadowrz/home-configuration.nix
@@ -148,6 +148,12 @@
           ]))
       ];
     };
+    ### GNU Emacs
+    emacs = {
+      enable = true;
+      package = pkgs.emacs-pgtk;
+      extraPackages = epkgs: with epkgs; [ treesit-grammars.with-all-grammars ];
+    };
     ### Firefox
     firefox = {
       enable = true;
