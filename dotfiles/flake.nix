74cf88fa03ab8507c0f957196a897484f97d7da5
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-11-21 16:43:40 +0800

    Update Dotfiles

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -9,30 +9,38 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      shared = [ ./home-configuration.nix ];
+      shared = import ./home-configuration.nix;
     in {
+      hmModules = {
+        inherit shared;
+        shadowrz = import ./shadowrz/home-configuration.nix;
+      };
       homeConfigurations = {
         root = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = shared ++ [{
-            home = {
-              username = "root";
-              homeDirectory = "/root";
-            };
-          }];
-        };
-        shadowrz = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = shared ++ [
-            ./shadowrz/home-configuration.nix
+          modules = [
+            self.hmModules.shared
             {
               home = {
                 username = "root";
                 homeDirectory = "/root";
+              };
+            }
+          ];
+        };
+        shadowrz = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            self.hmModules.shared
+            self.hmModules.shadowrz
+            {
+              home = {
+                username = "shadowrz";
+                homeDirectory = "/home/shadowrz";
               };
             }
           ];
