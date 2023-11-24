d8d35bab5b5b52eecf2ecbac7c609b22b5eebf23
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-11-24 18:12:28 +0800

    Update Blender

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -127,7 +127,7 @@
         description = "羽心印音";
         extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
         packages = with pkgs; [
-          blender_3_6 # Blender 3.6.* (Binary)
+          blender_4_0 # Blender 4.0.* (Binary)
           hugo # Hugo
           libsForQt5.plasma-sdk # Plasma SDK
           konversation
