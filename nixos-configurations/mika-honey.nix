bf4a196025a8658a4426aa6524e517ee41673391
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-01-17 11:25:52 +0800

    Update

--- nixos-configurations/mika-honey.nix
+++ nixos-configurations/mika-honey.nix
@@ -88,7 +88,10 @@
     };
   };
 
+  # VMware Guest
   virtualisation.vmware.guest.enable = true;
+  # XXX: Disable password requirement for wheel
+  security.sudo.wheelNeedsPassword = false;
 
   services = {
     # Getty
