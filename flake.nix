8e34b4fcf01d8eacec3fa792a6b36278202c5437
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-05-29 16:40:00 +0800

    refactor: Move to flake-parts

--- flake.nix
+++ flake.nix
@@ -4,12 +4,22 @@
   inputs = {
     # Nixpkgs
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    # Flake Utils
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    # Flake Parts
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Home Manager
+    # Disko
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -41,37 +51,53 @@
         home-manager.follows = "home-manager";
       };
     };
+    # Lanzaboote
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.3.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    # Treefmt
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # Users' flake
     berberman = {
       url = "github:berberman/flakes";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Lanzaboote
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    ### Dedupes
+    systems = { url = "github:nix-systems/default"; };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.treefmt-nix.flakeModule ];
 
-    nixosConfigurations = let modules = import ./nixos-modules.nix;
-    in {
-      mika-honey = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = modules.mika-honey;
-      };
-      mononekomi = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = modules.mononekomi;
+      systems = inputs.flake-utils.lib.defaultSystems;
+
+      flake = {
+
+        nixosConfigurations = let modules = import ./nixos-modules.nix;
+        in {
+          mika-honey = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+            modules = modules.mika-honey;
+          };
+          mononekomi = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+            modules = modules.mononekomi;
+          };
+        };
+
+        overlays.default = import ./pkgs/overlay.nix;
+
+        nixOnDroidConfigurations.default =
+          import ./nix-on-droid { inherit inputs; };
       };
     };
-
-    overlays.default = import ./pkgs/overlay.nix;
-
-    nixOnDroidConfigurations.default =
-      import ./nix-on-droid { inherit inputs; };
-  };
 }
