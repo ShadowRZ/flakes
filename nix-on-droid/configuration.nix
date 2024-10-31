bd41f44b1901eb77494377ec7b41436909aa5663
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-10-31 08:41:30 +0800

    nix-on-droid: Enable some tools

--- nix-on-droid/configuration.nix
+++ nix-on-droid/configuration.nix
@@ -105,6 +105,17 @@
     ];
   };
 
+  android-integration = {
+    am.enable = true;
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    xdg-open.enable = true;
+  };
+
   user.shell = lib.getExe pkgs.zsh;
   environment.sessionVariables."SHELL" = lib.getExe pkgs.zsh;
   environment.sessionVariables."PAGER" = lib.getExe pkgs.less;
