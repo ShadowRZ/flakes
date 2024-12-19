dab6e45c6d11fd1399881ac8e301275eedc29d9e
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-12-19 13:09:21 +0800

    home: Update

--- home/modules/vscode/default.nix
+++ home/modules/vscode/default.nix
@@ -3,8 +3,5 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    # CodeLLDB should use the Nixpkgs one for non FHS usage
-    extensions = [ pkgs.vscode-extensions.vadimcn.vscode-lldb ];
-    mutableExtensionsDir = true;
   };
 }
