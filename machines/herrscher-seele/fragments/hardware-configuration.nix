c7b740925ff845c9ad6df6e3cfcd3e27c214744a
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-01-09 12:40:46 +0800

    nixos: Enable NVIDIA power management

--- machines/herrscher-seele/fragments/hardware-configuration.nix
+++ machines/herrscher-seele/fragments/hardware-configuration.nix
@@ -24,6 +24,10 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
+      powerManagement = {
+        enable = true;
+        finegrained = false;
+      };
     };
     graphics = {
       extraPackages = with pkgs; [
