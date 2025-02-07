b3f1ac873df7a2023fb474eeed00622af9605dcf
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-02-07 23:43:21 +0800

    nix-on-droid: Disable direnv

--- nix-on-droid/flake-module.nix
+++ nix-on-droid/flake-module.nix
@@ -22,9 +22,15 @@
                     inputs.self.hmModules.default
                     inputs.self.hmModules.shell
                     inputs.nix-indexdb.hmModules.nix-index
+                    (
+                      { lib, ... }:
+                      {
+                        programs.nix-index-database.comma.enable = true;
+                        programs.direnv.enable = lib.mkForce false;
+                        manual.manpages.enable = false;
+                      }
+                    )
                   ];
-                  programs.nix-index-database.comma.enable = true;
-                  manual.manpages.enable = false;
                 };
               };
             }
