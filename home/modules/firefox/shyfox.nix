3dcaa5199498831307da493e58351eee7597be6c
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-03-10 14:10:45 +0800

    home(firefox): Bump ShyFox

--- home/modules/firefox/shyfox.nix
+++ home/modules/firefox/shyfox.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "ShyFox";
-  version = "a03da3d563b0abd8b8a15af0bc85ca8561e02b54";
+  version = "a22933f6c887a5cbfc098eb8565f147f6e718c66";
 
   src = fetchFromGitHub {
     owner = "ShadowRZ";
     repo = pname;
     rev = version;
-    hash = "sha256-afK32acu4zp6bHa+qnq4vstq3tsORHTnPdlrjZ9p5Vk=";
+    hash = "sha256-tYrsIPbgpqtSwD2UGcH5kbpD1J02os51ZZH9HbNm3CA=";
   };
 
   # Installs to $prefix/lib
