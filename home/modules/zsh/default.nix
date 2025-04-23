dc57bdabe3f4489a2ddbb5b9197dcbf70d2b1cdf
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-04-23 19:39:03 +0800

    home(zsh): Migrate options

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -39,43 +39,45 @@
       searchUpKey = [ "$key[Up]" ];
       searchDownKey = [ "$key[Down]" ];
     };
-    initExtraFirst = lib.mkBefore ''
-      # Subreap
-      { zmodload lilydjwg/subreap && subreap; } >/dev/null 2>&1
-      # Enable terminal cursor
-      ${pkgs.util-linux}/bin/setterm -cursor on
-      ${pkgs.coreutils}/bin/stty -ixon # Disable flow control
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        # Subreap
+        { zmodload lilydjwg/subreap && subreap; } >/dev/null 2>&1
+        # Enable terminal cursor
+        ${pkgs.util-linux}/bin/setterm -cursor on
+        ${pkgs.coreutils}/bin/stty -ixon # Disable flow control
 
-      setopt HIST_VERIFY
-      setopt HIST_FIND_NO_DUPS
-      setopt HIST_SAVE_NO_DUPS
-      setopt HIST_REDUCE_BLANKS
-      setopt EXTENDED_HISTORY
-      setopt INC_APPEND_HISTORY_TIME
-      setopt ALWAYS_TO_END
-      setopt LIST_PACKED
-      setopt COMPLETE_IN_WORD
-      setopt MENU_COMPLETE
-      setopt PUSHD_IGNORE_DUPS
-      setopt PUSHD_SILENT
-      setopt PUSHD_TO_HOME
-      setopt AUTO_PUSHD
-      setopt EXTENDED_GLOB
-      setopt MAGIC_EQUAL_SUBST
-      setopt NO_CLOBBER
-      setopt INTERACTIVE_COMMENTS
-      setopt RC_QUOTES
-      setopt CORRECT
-      setopt NO_FLOW_CONTROL
-      setopt TRANSIENT_RPROMPT
-      setopt NO_BEEP
-    '';
-    initExtra = with pkgs; ''
-      . ${oh-my-zsh}/share/oh-my-zsh/plugins/git/git.plugin.zsh
-      . ${zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh
+        setopt HIST_VERIFY
+        setopt HIST_FIND_NO_DUPS
+        setopt HIST_SAVE_NO_DUPS
+        setopt HIST_REDUCE_BLANKS
+        setopt EXTENDED_HISTORY
+        setopt INC_APPEND_HISTORY_TIME
+        setopt ALWAYS_TO_END
+        setopt LIST_PACKED
+        setopt COMPLETE_IN_WORD
+        setopt MENU_COMPLETE
+        setopt PUSHD_IGNORE_DUPS
+        setopt PUSHD_SILENT
+        setopt PUSHD_TO_HOME
+        setopt AUTO_PUSHD
+        setopt EXTENDED_GLOB
+        setopt MAGIC_EQUAL_SUBST
+        setopt NO_CLOBBER
+        setopt INTERACTIVE_COMMENTS
+        setopt RC_QUOTES
+        setopt CORRECT
+        setopt NO_FLOW_CONTROL
+        setopt TRANSIENT_RPROMPT
+        setopt NO_BEEP
+      '')
+      (with pkgs; ''
+        . ${oh-my-zsh}/share/oh-my-zsh/plugins/git/git.plugin.zsh
+        . ${zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh
 
-      ${builtins.readFile ./zinputrc.zsh}
-      ${builtins.readFile ./zshrc}
-    '';
+        ${builtins.readFile ./zinputrc.zsh}
+        ${builtins.readFile ./zshrc}
+      '')
+    ];
   };
 }
