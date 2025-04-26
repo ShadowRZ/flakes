b0c14864c2f424f7b6aab55ee253a7b317b0be68
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-04-26 13:57:10 +0800

    nixos: Allow Flake Registry

--- nixos/modules/nix/flakes.nix
+++ nixos/modules/nix/flakes.nix
@@ -7,13 +7,10 @@
 
   # Disable nix-channel
   nix.channel.enable = false;
-  # Disable flake-registry
-  nix.settings.flake-registry = "";
-  # Manually set NIX_PATH
-  nix.settings.nix-path = [ "nixpkgs=flake:nixpkgs" ];
+  nix.settings.flake-registry = "/etc/nix/registry.json";
 
-  nixpkgs.flake.setNixPath = false; # Set manually
-  nixpkgs.flake.setFlakeRegistry = false;
+  nixpkgs.flake.setNixPath = true;
+  nixpkgs.flake.setFlakeRegistry = true;
 
   # Does not work with Flake based configurations
   system.copySystemConfiguration = false;
