03d7a0a1ae7f1cc7236a9a3d18e999218487ff53
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-05-11 14:48:25 +0800

    nixos: Slightly tweak NVIDIA config

--- machines/herrscher-seele/fragments/hardware-configuration.nix
+++ machines/herrscher-seele/fragments/hardware-configuration.nix
@@ -13,14 +13,14 @@
       prime = {
         offload = {
           enable = true;
-          enableOffloadCmd = false;
+          enableOffloadCmd = true;
         };
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0@0:2:0";
+        nvidiaBusId = "PCI:1@0:0:0";
       };
       powerManagement = {
         enable = true;
-        finegrained = false;
+        finegrained = true;
       };
     };
     graphics = {
