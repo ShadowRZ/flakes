4ea7737ff45e82320637c0f221d784c523ac54d9
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-05-20 19:17:16 +0800

    home(firefox): Remove ShyFox

--- home/modules/firefox/default.nix
+++ home/modules/firefox/default.nix
@@ -52,111 +52,92 @@
       };
       # Profiles
       profiles = {
-        default =
-          {
-            name = "羽心印音";
-            settings = {
-              # Disable builtin MPRIS support in favor of Plasma Integration's one
-              "media.hardwaremediakeys.enabled" = false;
-              # Force enable account containers
-              "privacy.userContext.enabled" = true;
-              "privacy.userContext.ui.enabled" = true;
-              # Enable customChrome.css
-              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-              # Enable SVG context-propertes
-              "svg.context-properties.content.enabled" = true;
-              # Required by ShyFox
-              "sidebar.revamp" = false;
-              "layout.css.has-selector.enabled" = true;
-              "browser.urlbar.suggest.calculator" = true;
-              "browser.urlbar.unitConversion.enabled" = true;
-              "browser.urlbar.trimHttps" = true;
-              "browser.urlbar.trimURLs" = true;
-              "widget.gtk.rounded-bottom-corners.enabled" = true;
-              "widget.gtk.ignore-bogus-leave-notify" = "1";
-              # ShyFox
-              "shyfox.larger.context.menu" = true;
-              "shyfox.enable.ext.mono.toolbar.icons" = true;
-              "shyfox.enable.ext.mono.context.icons" = true;
-              "shyfox.enable.context.menu.icons" = true;
-              "shyfox.fill.accent.with.icons.fill.color" = true;
-              "shyfox.force.native.controls" = true;
-            };
-            # Firefox extensions
-            extensions.packages =
-              with pkgs.firefox-addons;
-              [
-                auto-tab-discard
-                behind-the-overlay-revival
-                bitwarden
-                clearurls
-                cliget
-                copy-selection-as-markdown
-                don-t-fuck-with-paste
-                fediact
-                firefox-color
-                ghosttext
-                link-gopher
-                linkhints
-                localcdn
-                multi-account-containers
-                no-pdf-download
-                offline-qr-code-generator
-                open-in-browser
-                plasma-integration
-                qr-code-address-bar
-                re-enable-right-click
-                react-devtools
-                reduxdevtools
-                side-view
-                sidebery
-                single-file
-                streetpass-for-mastodon
-                stylus
-                tabliss
-                tranquility-1
-                (ublock-origin.override rec {
-                  version = "1.61.2";
-                  url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.firefox.signed.xpi";
-                  sha256 = "sha256-7jpySkb/MsF9FyMHf+zG7ef9q3QhVAILUftiU93LuhQ=";
-                })
-                violentmonkey
-                vue-js-devtools
-                webhint
-              ]
-              ++ (
-                with (pkgs.callPackage ./addons.nix {
-                  inherit (pkgs.firefox-addons) buildFirefoxXpiAddon;
-                }); [
-                  # Additional
-                  copy-linktab-name-and-url
-                  custom-scrollbars
-                  emoji-sav
-                  foxyimage
-                  measure-it
-                  textarea-cache
-                  pwas-for-firefox
-                  userchrome-toggle-extended
-                ]);
-          }
-          // (
-            let
-              theme = pkgs.callPackage ./shyfox.nix { };
-            in
-            {
-              userChrome = ''
-                @import "${theme}/lib/shyfox/userChrome.css";
-
-                /* Minor tweaks */
-                :root, #screenshots-component * {
-                  --sdbr-wdt: 375px;
-                }
-              '';
-              userContent = ''
-                @import "${theme}/lib/shyfox/userContent.css";
-              '';
-            }
-          );
+        default = {
+          name = "羽心印音";
+          settings = {
+            # Disable builtin MPRIS support in favor of Plasma Integration's one
+            "media.hardwaremediakeys.enabled" = false;
+            # Force enable account containers
+            "privacy.userContext.enabled" = true;
+            "privacy.userContext.ui.enabled" = true;
+            # Enable customChrome.css
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            # Enable SVG context-propertes
+            "svg.context-properties.content.enabled" = true;
+            # Required by ShyFox
+            "sidebar.revamp" = false;
+            "layout.css.has-selector.enabled" = true;
+            "browser.urlbar.suggest.calculator" = true;
+            "browser.urlbar.unitConversion.enabled" = true;
+            "browser.urlbar.trimHttps" = true;
+            "browser.urlbar.trimURLs" = true;
+            "widget.gtk.rounded-bottom-corners.enabled" = true;
+            "widget.gtk.ignore-bogus-leave-notify" = "1";
+            # ShyFox
+            "shyfox.larger.context.menu" = true;
+            "shyfox.enable.ext.mono.toolbar.icons" = true;
+            "shyfox.enable.ext.mono.context.icons" = true;
+            "shyfox.enable.context.menu.icons" = true;
+            "shyfox.fill.accent.with.icons.fill.color" = true;
+            "shyfox.force.native.controls" = true;
+          };
+          # Firefox extensions
+          extensions.packages =
+            with pkgs.firefox-addons;
+            [
+              auto-tab-discard
+              behind-the-overlay-revival
+              bitwarden
+              clearurls
+              cliget
+              copy-selection-as-markdown
+              don-t-fuck-with-paste
+              fediact
+              firefox-color
+              ghosttext
+              link-gopher
+              linkhints
+              localcdn
+              multi-account-containers
+              no-pdf-download
+              offline-qr-code-generator
+              open-in-browser
+              plasma-integration
+              qr-code-address-bar
+              re-enable-right-click
+              react-devtools
+              reduxdevtools
+              side-view
+              sidebery
+              single-file
+              streetpass-for-mastodon
+              stylus
+              tabliss
+              tranquility-1
+              (ublock-origin.override rec {
+                version = "1.61.2";
+                url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.firefox.signed.xpi";
+                sha256 = "sha256-7jpySkb/MsF9FyMHf+zG7ef9q3QhVAILUftiU93LuhQ=";
+              })
+              violentmonkey
+              vue-js-devtools
+              webhint
+            ]
+            ++ (
+              with (pkgs.callPackage ./addons.nix {
+                inherit (pkgs.firefox-addons) buildFirefoxXpiAddon;
+              }); [
+                # Additional
+                copy-linktab-name-and-url
+                custom-scrollbars
+                emoji-sav
+                foxyimage
+                measure-it
+                textarea-cache
+                pwas-for-firefox
+                userchrome-toggle-extended
+              ]);
+        };
       };
     };
   };
