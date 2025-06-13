7765c31c940b5362c354cbeb79caa6dbfd0ee22f
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-06-13 10:35:53 +0800

    flakes: Add more Rust devShell

--- flake-module.nix
+++ flake-module.nix
@@ -34,21 +34,48 @@
         };
       };
 
-      devShells = {
-        rust = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-            rust-bin.stable.latest.default
-          ];
+      devShells =
+        let
+          mkRustDevShell =
+            rust-package:
+            pkgs.mkShell {
+              packages = with pkgs; [
+                openssl
+                pkg-config
+                rust-package
+              ];
+
+              RUST_SRC_PATH = "${rust-package}/lib/rustlib/src/rust/library";
+            };
+        in
+        {
+          rust =
+            mkRustDevShell (
+              pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              }
+            )
+            // builtins.mapAttrs (
+              version: toolchain:
+              toolchain.default.override {
+                extensions = [ "rust-src" ];
+              }
+            ) pkgs.rust-bin.stable;
+          rust-nightly =
+            mkRustDevShell (
+              pkgs.rust-bin.selectLatestNightlyWith (
+                toolchain:
+                toolchain.default.override {
+                  extensions = [ "rust-src" ];
+                }
+              )
+            )
+            // builtins.mapAttrs (
+              version: toolchain:
+              toolchain.default.override {
+                extensions = [ "rust-src" ];
+              }
+            ) pkgs.rust-bin.nightly;
         };
-        rust-nightly = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
-          ];
-        };
-      };
     };
 }
