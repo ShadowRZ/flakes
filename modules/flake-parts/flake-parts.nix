e970c803692ce5b57058dd0178244432dc3f7ea5
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-08-26 09:27:35 +0800

    chore: Add VS Code LSP configuration

--- modules/flake-parts/flake-parts.nix
+++ modules/flake-parts/flake-parts.nix
@@ -2,4 +2,8 @@
 {
   # Add Flake Parts flake module for modules feature.
   imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  # Enable debugging functionalities.
+  # Required for nixd auto completions.
+  debug = true;
 }
