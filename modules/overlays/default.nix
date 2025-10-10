1181eca9c3ad16a580def5cdae0b8eb1af7be477
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-10-10 18:49:14 +0800

    nixos: Remove local ltrace patch

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,16 +1,6 @@
 { inputs, ... }:
 {
   flake.overlays.default = final: prev: {
-    # https://github.com/NixOS/nixpkgs/pull/449551 TODO: Remove when merged
-    ltrace = prev.ltrace.overrideAttrs (attrs: {
-      patches = (attrs.patches or [ ]) ++ [
-        (final.fetchpatch {
-          name = "ltrace-0.7.3-print-test-pie.patch";
-          url = "https://raw.githubusercontent.com/gentoo/gentoo/refs/heads/master/dev-debug/ltrace/files/ltrace-0.7.3-print-test-pie.patch";
-          hash = "sha256-rUafTv13a4vS/yNIVRMbm5zwWTVTqMmFgmnS/XtPfdE=";
-        })
-      ];
-    });
     shadowrz = {
       firefox-addons = final.callPackage ../../firefox/addons.nix {
         inherit (final.firefox-addons) buildFirefoxXpiAddon;
