71419d2a55fcbad46b7d3fde36c30b28b143c08d
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-10-26 10:55:37 +0800

    nixos: Update

--- modules/base/read-only-pkgs.nix
+++ modules/base/read-only-pkgs.nix
@@ -3,6 +3,9 @@
   flake.modules.nixos = {
     base = {
       imports = [ inputs.nixpkgs.nixosModules.readOnlyPkgs ];
+
+      # TODO: Investigate
+      disabledModules = [ "hardware/facter/system.nix" ];
     };
   };
 }
