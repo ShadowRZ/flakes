d02a53141656865373b293688200b520eb8ceb07
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-02-11 09:51:44 +0800

    nixos: Use local blender-bin overlay instead

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,60 +1,66 @@
 { inputs, ... }:
 {
-  flake.overlays.default = final: _prev: {
-    shadowrz = {
-      silent-sddm = final.silent-sddm.override {
-        theme = "default";
-        extraBackgrounds = [ final.nixos-artwork.wallpapers.nineish.src ];
-        theme-overrides = {
-          "LoginScreen" = {
-            blur = 75;
-            background = "${final.nixos-artwork.wallpapers.nineish.src.name}";
-          };
-          "LockScreen" = {
-            blur = 0;
-            background = "${final.nixos-artwork.wallpapers.nineish.src.name}";
-          };
-          "LockScreen.Clock" = {
-            font-family = "Space Grotesk";
-          };
-          "LockScreen.Date" = {
-            font-family = "Space Grotesk";
-          };
-          "LockScreen.Message" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.LoginArea.Username" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.LoginArea.PasswordInput" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.LoginArea.LoginButton" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.LoginArea.Spinner" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.LoginArea.WarningMessage" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.MenuArea.Buttons" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.MenuArea.Popups" = {
-            font-family = "Space Grotesk";
-          };
-          "Tooltips" = {
-            font-family = "Space Grotesk";
-          };
-          "LoginScreen.MenuArea.Layout" = {
-            display = false;
+  flake.overlays.default =
+    final: _prev:
+    {
+      shadowrz = {
+        silent-sddm = final.silent-sddm.override {
+          theme = "default";
+          extraBackgrounds = [ final.nixos-artwork.wallpapers.nineish.src ];
+          theme-overrides = {
+            "LoginScreen" = {
+              blur = 75;
+              background = "${final.nixos-artwork.wallpapers.nineish.src.name}";
+            };
+            "LockScreen" = {
+              blur = 0;
+              background = "${final.nixos-artwork.wallpapers.nineish.src.name}";
+            };
+            "LockScreen.Clock" = {
+              font-family = "Space Grotesk";
+            };
+            "LockScreen.Date" = {
+              font-family = "Space Grotesk";
+            };
+            "LockScreen.Message" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.LoginArea.Username" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.LoginArea.PasswordInput" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.LoginArea.LoginButton" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.LoginArea.Spinner" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.LoginArea.WarningMessage" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.MenuArea.Buttons" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.MenuArea.Popups" = {
+              font-family = "Space Grotesk";
+            };
+            "Tooltips" = {
+              font-family = "Space Grotesk";
+            };
+            "LoginScreen.MenuArea.Layout" = {
+              display = false;
+            };
           };
         };
       };
+      silent-sddm = final.callPackage inputs.silent-sddm {
+        gitRev = inputs.silent-sddm.rev;
+      };
+    }
+    // inputs.nixpkgs.lib.filesystem.packagesFromDirectoryRecursive {
+      inherit (final) callPackage;
+      directory = ../../overlays;
     };
-    silent-sddm = final.callPackage inputs.silent-sddm {
-      gitRev = inputs.silent-sddm.rev;
-    };
-  };
 }
