b17fd129bf322c45e0c816a4440740ff061f03cf
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-05-05 15:08:52 +0800

    feat(desktop): Build all locales

--- modules/hosts/mimeow-coffees/default.nix
+++ modules/hosts/mimeow-coffees/default.nix
@@ -16,9 +16,7 @@
         # Build all Glibc supported locales as defined in:
         # https://sourceware.org/git/?p=glibc.git;a=blob;f=localedata/SUPPORTED
         supportedLocales = [
-          "C.UTF-8/UTF-8"
-          "en_US.UTF-8/UTF-8"
-          "zh_CN.UTF-8/UTF-8"
+          "all"
         ];
       };
 
